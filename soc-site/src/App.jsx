57d71f618d60998b18405019e1c46ea984ad9d7e
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2023-11-23 23:18:52 +0400

    start
    start

---
+++ soc-site/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import Masiv from './masivi/masivi'
+
+
+
+function App() {
+ 
+ 
+}
+
+export default App
---
+++ soc-site/src/masivi/masivi.jsx
@@ -0,0 +1,38 @@
+
+const Masiv = ()=> {
+    const loginPage = [
+      {
+        logo: [
+            {
+                imgLogo:"./src/masivi/foto/geo-logo.png",
+                imgFon: "./src/masivi/foto/regiter-background.jpg",
+                imgName:"Geo.ge"
+            }
+        ]
+      },
+
+      {
+        buttons:[
+            {
+                login: "შესვლა",
+                register: "რეგისტრაცია"
+            }
+        ]
+      },
+
+      {
+        loginPageInfo: [
+            {
+                title: "Geo.ge - ქართული სოციალური საიტი",
+                info: "საიტი შექმნილია ისე რომ მაქსილამულარ დაიცვას მომხმარებელთა კომფიდენციალობის პოლიტიკა, საიტი განკუთვნილია ნებისმიერი ასაკის მომხმარებლიოსთვის. Geo.ge არის პირველი ქართული სოციალური საიტი შექმნილი მარტივი კომუნიკაციისა და ახალი ურთიერთობებისათვის."
+            }
+        ]
+      }
+        
+          
+    ];
+
+    console.log(loginPage)
+}
+
+export default Masiv
